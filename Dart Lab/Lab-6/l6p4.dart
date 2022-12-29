b1ccdbbd68f1d56646ce64a5f4201b291727da30
Vraj Raval <93474512+Vraj-Raval@users.noreply.github.com>
2022-12-29 19:57:24 +0530

    Update l6p4.dart

--- Dart Lab/Lab-6/l6p4.dart
+++ Dart Lab/Lab-6/l6p4.dart
@@ -1,18 +1,38 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  List<String> a = [];
-  List<String> b = [];
-  print("How many Phone numbers do you want to save:");
-  int n = int.parse(stdin.readLineSync()!);
-  for (int i = 0; i < n; i++) {
-    print("Enter the name");
-    a.add(stdin.readLineSync()!);
-    print("Enter the number");
-    b.add(stdin.readLineSync()!);
+  PhoneBook()..createContact();
+}
+
+class PhoneBook {
+  List<Map<String, dynamic>> phone_book = [];
+
+  void createContact() {
+    while (true) {
+      stdout.write("Enter your choice:\n1.Create a contact\n2.Info\n3.Exit");
+      int choice = int.parse(stdin.readLineSync()!);
+      if (choice == 1) {
+        Map<String, dynamic> map = {};
+
+        stdout.write("Enter NAME:");
+        map['Name'] = stdin.readLineSync()!;
+        stdout.write("MobileNo:");
+        map['mobileno'] = stdin.readLineSync()!;
+        phone_book.add(map);
+      } else if (choice == 2) {
+        info();
+      } else if (choice == 3) {
+        break;
+      } else {
+        print("Enter Valid Choice 1,2 or 3");
+      }
+    }
   }
-  for (var i = 0; i < n; i++) {
-    stdout.write(a[i] + "-" + b[i]);
-    stdout.write("\n");
+
+  void info() {
+    for (int i = 0; i < phone_book.length; i++) {
+      print(
+          "Name:${phone_book[i]['Name']}\nMobile No:${phone_book[i]['mobileno']}");
+    }
   }
 }
